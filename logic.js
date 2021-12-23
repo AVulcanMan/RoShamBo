b7ad33054a7a12941d25d57a2d2c0f9e19f32f4d
YashasPatil <yashaspatil@gmail.com>
2021-12-23 15:07:13 -0800

    Broken game logic

--- logic.js
+++ logic.js
@@ -19,7 +19,7 @@ function computerPlay () {
     }
     return (compChoice);
 }
-const computerSelection= computerPlay();
+
 
 let decision;
 function playerPlay () {
@@ -27,4 +27,25 @@ function playerPlay () {
     decision= decision.toLowerCase();
     return (decision);
 }
-const playerSelection= playerPlay();
+
+
+function playRound () {
+    let playerSelection= playerPlay();
+    let computerSelection= computerPlay();
+    if (playerSelection=='rock' && computerSelection=='scissors') {
+        return("You Win! Rock beats scissors");
+    } else if (playerSelection=='scissors' && computerSelection=='rock'){
+        return ("You lose. Rock beats scissors.");
+    } else if (playerSelection=='scissors' && computerSelection=='paper'){
+        return ("You win! Scissors beats paper");
+    } else if (playerSelection=='paper' && computerSelection=='scissors'){
+        return ("You lose. Scissors beats paper.");
+    } else if (playerSelection=='paper' && computerSelection=='rock'){
+        return ("You win! Paper beats rock");
+    } else if (playerSelection=='rock' && computerSelection=='paper'){
+        return ("You lose. Paper beats rock.");
+    } else {
+        return ("It's a tie!");
+    }
+}
+
