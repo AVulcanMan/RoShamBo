5af2e5c8801a80be9b7386a7da02d9e3e66980de
YashasPatil <yashaspatil@gmail.com>
2021-12-23 13:23:47 -0800

    Randomly select rock, paper, or scissors

--- logic.js
+++ logic.js
@@ -5,3 +5,16 @@ console.log("Hello World!");
 - Take selections from both and determine a valid result
 - Output the result
 */
+let compChoice;
+let num;
+function computerPlay () {
+    num= Math.floor (Math.random()*3+1);
+    if (num==1){
+        compChoice= 'rock';
+    } else if (num==2) {
+        compChoice='scissors';
+    } else {
+        compChoice='paper';
+    }
+    return (compChoice);
+}
