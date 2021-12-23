5988f57444cf9aba7a2908f7e862a017576f4872
YashasPatil <yashaspatil@gmail.com>
2021-12-23 15:27:46 -0800

    Added a way to play 5 round matches

--- logic.js
+++ logic.js
@@ -32,21 +32,37 @@ function playerPlay () {
 function playRound () {
     let playerSelection= playerPlay();
     let computerSelection= computerPlay();
+    let playerScore=0;
+    let compScore=0;
     console.log(playerSelection, computerSelection);
     if (playerSelection=='rock' && computerSelection=='scissors') {
         return("You Win! Rock beats scissors");
+        playerScore++;
     } else if (playerSelection=='scissors' && computerSelection=='rock'){
         return ("You lose. Rock beats scissors.");
+        compScore++;
     } else if (playerSelection=='scissors' && computerSelection=='paper'){
         return ("You win! Scissors beats paper");
+        playerScore++;
     } else if (playerSelection=='paper' && computerSelection=='scissors'){
         return ("You lose. Scissors beats paper.");
+        compScore++;
     } else if (playerSelection=='paper' && computerSelection=='rock'){
         return ("You win! Paper beats rock");
+        playerScore++;
     } else if (playerSelection=='rock' && computerSelection=='paper'){
         return ("You lose. Paper beats rock.");
+        compScore++;
     } else {
         return ("It's a tie!");
     }
 }
 
+
+function game () {
+    for (var i=0; i<5; i++){
+        console.log(playRound());
+    }
+   console.log(`${playerScore} - ${compScore}`);
+}
+game();
