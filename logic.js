275748c334b40dcd84f1b21e62374fc3447df289
YashasPatil <yashaspatil@gmail.com>
2021-12-23 12:47:22 -0800

    Mapped out subproblems

--- logic.js
+++ logic.js
@@ -1,1 +1,7 @@
 console.log("Hello World!");
+/* Subproblems
+- Have a computer generate rock, paper, or scissors
+- Make sure console accepts VALID user inputs
+- Take selections from both and determine a valid result
+- Output the result
+*/
